2b46bfb9469d18cc4318f6185bc75dc08244105e
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-09-27 12:38:44 +0300

    Update Занятие 3 Задание 7.py

--- Занятие 3 Задание 7.py
+++ Занятие 3 Задание 7.py
@@ -1,6 +1,5 @@
-g = int(input())
-if (g % 4 == 0 and g % 100 != 0) or g % 400 == 0:
-    print("Да")
-else:
-    print("Нет")
-    
+def add(g):
+    if (g % 4 == 0 and g % 100 != 0) or g % 400 == 0:
+        print("Да")
+    else:
+        print("Нет")

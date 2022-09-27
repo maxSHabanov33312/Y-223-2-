f3858acd40488e86b222d3582f4de7e8a00908af
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-09-27 12:59:00 +0300

    Update Занятие 3 Задание 9.py

--- Занятие 3 Задание 9.py
+++ Занятие 3 Задание 9.py
@@ -1,8 +1,6 @@
-n = int(input())
-m = int(input())
-k = int(input())
-s = n*m 
-if s > k and ((k % n == 0) or (k % m == 0)):
-    print("Да")
-else:
-    print("Нет")
+def add(n,m,k):
+    s = n*m
+    if s > k and ((k%n == 0) or (k % m == 0)):
+        print("Да")
+    else:
+        print("Нет")

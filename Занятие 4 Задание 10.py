821538a517b3f7afab5c83fc489feae518e81792
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-13 23:15:34 +0300

    Update Занятие 4 Задание 10.py

--- Занятие 4 Задание 10.py
+++ Занятие 4 Задание 10.py
@@ -1,9 +1,18 @@
-N = int(input("Введите длину ряда: ")) #Требуется доработка
-K = int(input())
-f1 = f2 = 1
-for i in list(range(2, N)):
-    f1, f2 = f2, f1 + f2
-    print(f1, f2, end=" ")
-    x = K in list(range(2, N))
-    y = N in list(range(2, N))
-    print(sum(range(x, y)))
+def Fubonacchi(Z, K, N): #Нуждается в доработке
+    X = 0
+    list = []
+    a = 1
+    y = 0
+    for i in range(1, Z):
+        b = a
+        a = b + y
+        y = b
+        list.append(b)
+    list.insert(0, 0)
+    print(list)
+    repeat = N - K
+    for i in range(repeat + 1):
+        znach = list[K + i]
+        X += znach
+    print(X)
+Fubonacchi(Z = int(input()), K = int(input()), N = int(input()))

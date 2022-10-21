4f2fca23665e0f9f79e36297f36ec9cccde21ed9
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-21 13:50:24 +0300

    Update Занятие 4 Задание 1.py

--- Занятие 4 Задание 1.py
+++ Занятие 4 Задание 1.py
@@ -1,8 +1,8 @@
-A = int(input())
-B = int(input())
-C = B + 1
-if A <= C:
-    for i in range(A,C):
-        print(i,end=";")
-else:
-    print("Условие неверно")
+def F(A,B):
+    C = B + 1
+    if A <= C:
+        for i in range(A,C):
+            print(i,end=";")
+    else:
+        print("Условие неверно")
+F(A = int(input()),B = int(input()))

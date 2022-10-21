1d285f0abe6216ea772d8f53f122f5ac9a975b60
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-21 13:57:44 +0300

    Update Занятие 4 Задание 3.py

--- Занятие 4 Задание 3.py
+++ Занятие 4 Задание 3.py
@@ -1,8 +1,8 @@
-A = int(input())
-B = int(input())
-if A>B:
-    for i in range(A,B - 1,-1):
-        if i%2!=0:
-            print(i,end="; ")
-else:
-    print("Неправильные числа")
+def F(A,B):
+    if A>B:
+        for i in range(A,B - 1,-1):
+            if i%2!=0:
+                print(i,end="; ")
+    else:
+        print("Неправильные числа")
+F(A = int(input()),B = int(input()))

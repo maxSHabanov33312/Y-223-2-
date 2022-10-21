81ed8eab2db19e7271be69257d057130ec8ab6fa
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-21 14:22:30 +0300

    Update Занятие 4 Задание 7.py

--- Занятие 4 Задание 7.py
+++ Занятие 4 Задание 7.py
@@ -1,7 +1,8 @@
-n = int(input())
-a = 1
-b = 1
-for i in range(2,n+1):
-    a = a * i
-    b += a
-print(b)
+def F(n):
+    a = 1
+    b = 1
+    for i in range(2,n+1):
+        a = a * i
+        b += a
+    print(b)
+F(n = int(input()))

408090f41151915288a6fc5cba2f8cf0dfb7e600
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-21 14:12:01 +0300

    Update Занятие 4 Задание 4.py

--- Занятие 4 Задание 4.py
+++ Занятие 4 Задание 4.py
@@ -1,7 +1,8 @@
-sum = 0
-N = int(input())
-for i in range(N):
-    n = int(input())
-    sum += n
-print(N)
-print(sum)
+def F(N,n):
+    sum = n
+    for i in range(N-1):
+        n = int(input())
+        sum += n
+    print(N)
+    print(sum)     
+F(N = int(input()),n = int(input()))

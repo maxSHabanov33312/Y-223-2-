be95e6eedcfcfbfbaba028127cb91f0a06da3f0b
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-21 14:25:12 +0300

    Update Занятие 4 Задание 8.py

--- Занятие 4 Задание 8.py
+++ Занятие 4 Задание 8.py
@@ -1,8 +1,9 @@
-n = int(input())
-if n<=9:
-    for i in range(1,n+1):
-        for s in range(1,i+1):
-            print(s,end=" ")
-        print()
-else:
-    print("Неверное значение")
+def F(n):
+    if n<=9:
+        for i in range(1,n+1):
+            for s in range(1,i+1):
+               print(s,end=" ")
+            print()
+    else:
+        print("Неверное значение")
+F(n = int(input()))

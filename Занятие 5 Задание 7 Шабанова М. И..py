1cf3ab845f5e5ae16443ecb893f55eb69cdf5acf
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-22 13:14:03 +0300

    Update Занятие 5 Задание 7 Шабанова М. И..py

--- Занятие 5 Задание 7 Шабанова М. И..py
+++ Занятие 5 Задание 7 Шабанова М. И..py
@@ -1,11 +1,12 @@
-x = int(input('Введите целые неотрицательные числа, '
-              'для завершения введите 0:\n>> '))
-lst = [x]
-y = 0
-while x != 0:
-    lst.append(x)
-    x = int(input('>> '))
-for i in range(len(lst)-1):
-    if lst[i] < lst[i+1]:
-        y += 1
-print(y)
+def F(x):   
+    list = [x]
+    y = 0
+    while x != 0:
+        list.append(x)
+        x = int(input('>> '))
+    for i in range(len(list)-1):
+        if list[i] < list[i+1]:
+            y += 1
+    print(y)
+F(x = int(input('Введите целые неотрицательные числа, '
+              'для завершения введите 0:\n>> ')))

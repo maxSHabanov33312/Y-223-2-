b37d6a9089bd65194665fa94d80765c776348f3e
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-22 12:43:16 +0300

    Update Занятие 5 Задание 2 Шабанова М. И..py

--- Занятие 5 Задание 2 Шабанова М. И..py
+++ Занятие 5 Задание 2 Шабанова М. И..py
@@ -1,5 +1,9 @@
-n = int(input())
-i = 2
-while n%i != 0:
-    i += 1
-print(i)
+def F(n):
+    i = 2
+    if n > 2: 
+        while n%i != 0:
+            i += 1
+        print(i)
+    else:
+        print("Неверное число!")
+F(n = int(input()))

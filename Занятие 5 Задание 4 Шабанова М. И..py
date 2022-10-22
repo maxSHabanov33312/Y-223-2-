f48241e8e5881e022e7472ed7a96bc2e5323ff2d
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-22 12:52:28 +0300

    Update Занятие 5 Задание 4 Шабанова М. И..py

--- Занятие 5 Задание 4 Шабанова М. И..py
+++ Занятие 5 Задание 4 Шабанова М. И..py
@@ -1,7 +1,7 @@
-x = int(input())
-y = int(input())
-d = 1
-while x < y:
-    x = x*1.1
-    d+=1
-print(d)
+def F(x,y):
+    d = 1
+    while x < y:
+        x = x*1.1
+        d+=1
+    print(d)
+F(x = int(input()),y = int(input()))

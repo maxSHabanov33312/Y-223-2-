737a769901c1aa916d07f692d8fd7cae3320d582
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-22 12:36:39 +0300

    Update Занятие 5 Задание 1 Шабанова М. И..py

--- Занятие 5 Задание 1 Шабанова М. И..py
+++ Занятие 5 Задание 1 Шабанова М. И..py
@@ -1,7 +1,12 @@
-N = int(input())
-i = 1
-x = 1
-while N >= x:
-    x = i**2
-    i += 1
-    print(x)
+def F(N):
+    i = 1
+    x = 1
+    C = N - 4
+    while C >= x:
+        if N > x:
+            x = i**2
+            i += 1
+            print(x)
+        else:
+            break
+F(N = int(input()))

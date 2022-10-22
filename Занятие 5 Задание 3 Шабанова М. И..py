23ee52ebfc5be3d3ad7c2941f7e9ffaa89669937
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-22 12:48:46 +0300

    Update Занятие 5 Задание 3 Шабанова М. И..py

--- Занятие 5 Задание 3 Шабанова М. И..py
+++ Занятие 5 Задание 3 Шабанова М. И..py
@@ -1,6 +1,6 @@
-n = int(input()).bit_length()-1
-a = 1
-for i in range(n):
-    a *= 2
-
-print(n, a)
+def F(n):
+    a = 1
+    for i in range(n):
+        a *= 2
+    print(n, a)
+F(n = int(input()).bit_length()-1)

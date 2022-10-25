db032949db1e75e45268ca361845dea053c6870c
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-10-25 12:44:28 +0300

    Update Занятие 7 Вариант 13 Задание 1.py

--- Занятие 7 Вариант 13 Задание 1.py
+++ Занятие 7 Вариант 13 Задание 1.py
@@ -1,14 +1,15 @@
-def F(n):   
+def F(n):
     a = []
     b = []
+    c = []
     for i in range(n):
         print("Введите", i, "элемент")
         a.append(int(input()))
     print(a)
-    for i in a:
-        if a.count(i) > 1:
-            b.append(i)
-    c = a.index(a[i], a.count(i) > 1) 
+    for i, x in enumerate(a):
+        if x in a[:i]:
+            b.append(x)
+            c.append(i)
     print(b)
     print(c)
 F(n = int(input("Введите длину массива ")))

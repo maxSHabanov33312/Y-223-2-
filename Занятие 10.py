ad47a8c943e1d50c951afc502829b0021029da89
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-11-15 01:56:11 +0300

    Update Занятие 10.py

--- Занятие 10.py
+++ Занятие 10.py
@@ -1,4 +1,3 @@
-print('задача 1')
 with open(r'C:\\Users\\USER\\Desktop\\Шабанов М.И._У-223_vvod1.txt', encoding='utf-8') as w:
     print(w.read())
 with open(r'C:\\Users\\USER\\Desktop\\Шабанов М.И._У-223_vivod1.txt', encoding='utf-8') as w:

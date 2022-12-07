328b4ee04efcb4faa81849c5b4ce2bc302203a1d
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-12-07 22:25:06 +0300

    Update Занятие 12 блок А(6).py

--- Занятие 12 блок А(6).py
+++ Занятие 12 блок А(6).py
@@ -1,9 +1,13 @@
-import random
-def f(n):
-    s = random.randint(2,n)
-    if n > 1:
-        if n%1==0 and n%n==0 and n%s==0:
-            return f(n) 
-        else:
-            print("YES")                                 
-f(n = int(input("Введите число больше 1: ")))
+def f(n, s=3):
+    if n == 2:  
+        x = print("YES")
+    elif n <= 1 or n % 2 == 0:  
+        x = print("NO")
+    elif s * s > n:  
+        x = print("YES")
+    elif (n % s) == 0:  
+        x = print("NO")
+    else:  
+        x = f(n, s+2)
+    return x
+f(n = int(input("Введите число больше 1: ")), s = 2)

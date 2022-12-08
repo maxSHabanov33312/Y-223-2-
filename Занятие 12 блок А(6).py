aee83cb0759ce38d977fe9abb529c0d1d76e06cb
Maxim <113377679+maxSHabanov33312@users.noreply.github.com>
2022-12-08 17:22:09 +0300

    Update Занятие 12 блок А(6).py

--- Занятие 12 блок А(6).py
+++ Занятие 12 блок А(6).py
@@ -1,13 +1,20 @@
-def f(n, s=3):
-    if n == 2:  
-        x = print("YES")
-    elif n <= 1 or n % 2 == 0:  
-        x = print("NO")
-    elif s * s > n:  
-        x = print("YES")
-    elif (n % s) == 0:  
-        x = print("NO")
-    else:  
-        x = f(n, s+2)
+def f(n, s):
+    x  = 0
+    if n <= 1:
+        x = print("Некоректное число")
+    else:
+        if n == 2:  
+            x = print("YES")
+        else:
+            if n <= 1 or n % 2 == 0:
+                x = print("NO")
+            else:
+                if s * s > n:
+                    x = print("YES")
+                else:
+                    if (n % s) == 0:  
+                        x = print("NO")
+                    else:  
+                        x = f(n, s+2)
     return x
-f(n = int(input("Введите число больше 1: ")), s = 2)
+f(n = int(input("Введите число больше 1: ")), s = 3)
